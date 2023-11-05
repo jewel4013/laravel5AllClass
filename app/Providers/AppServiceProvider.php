fd54381c3b7763c09c1ecf61262a58a58acc05de
Jewel Office <jewel.rana.4013@gmail.com>
2023-11-05 15:46:53 +0600

    Fresh branch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Relation::morphMap([
-            'Status' => Status::class,
-            'Video' => Video::class,
-        ]);
+        
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,28 +11,8 @@
     <div class="container">
         <h1>It's our Home Page</h1>
 
-        <div class="">
-            <a href="/authors" class="btn btn-primary">Authors</a>
-            <a href="/books" class="btn btn-primary">Books</a>
-            <a href="/country" class="btn btn-primary">Country</a>
-            <a href="/person" class="btn btn-primary">Person</a>
-            <a href="/users" class="btn btn-primary">User</a>
-            <a href="/mailing" class="btn btn-primary">Sent mail</a>
-            <a href="/files" class="btn btn-primary">File upload</a>
-            <a href="/morph" class="btn btn-primary">Morph Relations</a>
-            <a href="/sessionput" class="btn btn-primary">Session_Put</a>
-            <a href="/sessionpull" class="btn btn-primary">Session_Pull</a>
-            <a href="/login" class="btn btn-primary">Log In</a>
-            <a href="/logout" class="btn btn-primary">Log out</a>
-        </div>
-
-
-        {{-- <p>{{ session()->get('user_name', 'No Session create') }}</p> --}}
-        {{-- <p>{{ request()->session()->get('user_name', 'No Session create') }}</p> --}}
-        {{-- <P>{{ session()->has('user_name') ? 'Yes' : 'No' }}</P>
-        <P>{{ session()->exists('user_name') ? 'Yes' : 'No' }}</P> --}}
-        <P>{{ session()->exists('user') ? 'Yes' : 'No' }}</P>
         
+
 
 
 
